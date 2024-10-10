ed150e202352d119b0340dbecf80a2e7d7db0cbc
Abdul Sharif <abdul.sharif@gmail.com>
2024-10-10 01:46:12 +0000

    python session 1

---
+++ python/function.py
@@ -0,0 +1,9 @@
+def sum(a, b):
+    result = a + b
+    return result
+
+arg1 = int(input("Please enter first number: "))
+arg2 = int(input("Please enter second number: "))
+
+result = sum(a=arg1, b=arg2)
+print(result)
---
+++ python/helloworld.py
@@ -0,0 +1,23 @@
+## function(argument)
+
+message = "Hello World"
+print(message)
+
+if 5 > 2:
+    print("5 is greater than 2")
+    print("Test print")
+elif 10 > 5:
+     print("10 is greater than 5")
+else:
+    print("Something is wrong")
+
+for item in ["Abdul", "Akmal", "Ahmad"]:
+    if item == "Abdul":
+        name = "Abdul"
+
+x = 0
+while x < 5:
+    print("Welcome", name, x, "times")
+    x += 1 
+
+
